07c2f85c7bfc1b3dd9d70526a4f7bfc331b9feaa
Bui Duc Huy <duchuy.124dk@gmail.com>
2022-03-16 10:59:54 +0700

    feat(contract): check caller of register is creater of contract

--- contracts/EnableGasPayer.sol
+++ contracts/EnableGasPayer.sol
@@ -16,11 +16,41 @@ contract EnableGasPayer {
   mapping(address => address) public _payers;
   uint256 public LOCK_VALUE;
 
+  function _getAddressCreate(address _creator, uint _nonce) private view returns(address) {
+    bytes memory data;
+    if (_nonce == 0x00) {
+        data = abi.encodePacked(byte(0xd6), byte(0x94), _creator, byte(0x80));
+    } else if (_nonce <= 0x7f) {
+        data = abi.encodePacked(byte(0xd6), byte(0x94), _creator, uint8(_nonce));
+    } else if (_nonce <= 0xff) {
+        data = abi.encodePacked(byte(0xd7), byte(0x94), _creator, byte(0x81), uint8(_nonce));
+    } else if (_nonce <= 0xffff) {
+        data = abi.encodePacked(byte(0xd8), byte(0x94), _creator, byte(0x82), uint16(_nonce));
+    } else if (_nonce <= 0xffffff) {
+        data = abi.encodePacked(byte(0xd9), byte(0x94), _creator, byte(0x83), uint24(_nonce));
+    } else {
+        data = abi.encodePacked(byte(0xda), byte(0x94), _creator, byte(0x84), uint32(_nonce));
+    }
+    return address(uint256(keccak256(data)));
+  }
+
+  function _getAddressCreate2(address _creator, bytes32 _codeHash, uint256 _salt) private view returns(address) {
+      return address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, _codeHash)))));
+  }
+
   function setLockValue(uint256 _lockValue) public {
     LOCK_VALUE = _lockValue;
   }
 
-  function register(address _contract) payable public {
+  function register(address _contract, bytes32 _codeHash, uint256 _nonce, bool _isCreate2) payable public {
+    address contractAddress;
+    if (_isCreate2) {
+      contractAddress = _getAddressCreate2(msg.sender, _codeHash, _nonce);
+    } else {
+      contractAddress = _getAddressCreate(msg.sender, _nonce);
+    }
+
+    require(_contract == contractAddress, "Coin98 EnableGasPayer: Contract address invalid");
     require(_enableContracts[_contract] == false, "Coin98 EnableGasPayer: Contract Is Enable");
     require(msg.value == LOCK_VALUE, "Coin98 EnableGasPayer: Exceed Value");
     _enableContracts[_contract] = true;
